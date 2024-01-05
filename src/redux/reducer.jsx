0402efe0f65e02330f86fc5a51c5a87dcc859008
vadik184 <133372327+vadik184@users.noreply.github.com>
2024-01-05 13:50:41 +0200

    up2

--- src/redux/reducer.jsx
+++
@@ -1,7 +0,0 @@
-import { filtersReducer } from './filterSlice';
-import { contactsReducer } from './contactSlice';
-
-export const reducer = {
-  contacts: contactsReducer,
-  filter: filtersReducer,
-};
